950d406c2fcaef07d56d06d634a12ec9a691bef6
satyamx64 <satyamc64@gmail.com>
2022-05-16 05:47:46 +0530

    integrated firebase auth service

--- lib/src/presentation/views/home/home_view.dart
+++ lib/src/presentation/views/home/home_view.dart
@@ -1,15 +1,110 @@
+import 'package:defi/src/presentation/shared/utils/color.dart';
+import 'package:defi/src/services/auth/repo/auth_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final authRepository = context.read<AuthRepository>();
     return Scaffold(
-      body: Center(
-        child: Text('Auth Screen'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Satyam',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                  const Spacer(),
+                  const Text(
+                    '10000',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18),
+                    child: SizedBox(
+                        height: 22,
+                        width: 22,
+                        child: Image.asset('assets/coin.png')),
+                  )
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 32),
+                height: 420,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.white30,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.maxFinite,
+                height: 54,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorUtils.primary),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Pay',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                width: double.maxFinite,
+                height: 54,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorUtils.primary),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Recieve',
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
       ),
     );
   }
--- lib/src/services/auth/services/auth_service.dart
+++ lib/src/services/auth/services/auth_service.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 import '../../../models/user.dart';
 
 abstract class AuthService {
-  factory AuthService() => _MockService();
+  factory AuthService() => _FirebaseAuthService();
   Future<void> signInWithGoogle();
 
   Future<void> createUserWithEmailAndPassword({
@@ -92,4 +94,100 @@ class AuthException implements Exception {
 
   @override
   String toString() => message;
+}
+
+class _FirebaseAuthService implements AuthService {
+  final auth = FirebaseAuth.instance;
+  @override
+  Future<void> createUserWithEmailAndPassword(
+      {required String email,
+      required String password,
+      String name = ''}) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw AuthException(message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw AuthException(
+            message: 'The account already exists for that email.');
+      } else if (e.code == 'invalid-email') {
+        throw AuthException(message: 'The email provided is invalid.');
+      } else if (e.code == 'operation-not-allowed') {
+        throw AuthException(message: 'Unsupported Operation');
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  UserModel? get currentUser {
+    return UserModel.fromMap(
+        {'email': auth.currentUser!.email, 'id': auth.currentUser!.uid});
+  }
+
+  @override
+  Future<void> forgotPassword({required String email}) async {
+    try {
+      await auth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      if (e.code == 'invalid-email') {
+        throw AuthException(message: 'The email provided is Invalid.');
+      } else if (e.code == 'missing-android-pkg-name') {
+        print('e.code : ${e.message}');
+        throw AuthException(message: 'Something went wrong');
+      } else if (e.code == 'missing-continue-uri') {
+        print('e.code : ${e.message}');
+        throw AuthException(message: 'Something went wrong');
+      } else if (e.code == 'missing-ios-bundle-id') {
+        print('e.code : ${e.message}');
+        throw AuthException(message: 'Something went wrong');
+      } else if (e.code == 'invalid-continue-uri') {
+        print('e.code : ${e.message}');
+        throw AuthException(message: 'Something went wrong');
+      } else if (e.code == 'unauthorized-continue-uri') {
+        print('e.code : ${e.message}');
+        throw AuthException(message: 'Something went wrong');
+      } else if (e.code == 'user-not-found') {
+        throw AuthException(message: 'No user associated with provided email.');
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<bool> get isSignedIn {
+    return Future.value(auth.currentUser != null);
+  }
+
+  @override
+  Future<void> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'wrong-password') {
+        throw AuthException(message: 'Wrong Password.');
+      } else if (e.code == 'user-not-found') {
+        throw AuthException(message: 'No user associated with provided email');
+      } else if (e.code == 'user-disabled') {
+        throw AuthException(message: 'The account is disabled.');
+      } else if (e.code == 'invalid-email') {
+        throw AuthException(message: 'The email provided is invalid.');
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signInWithGoogle() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() async {
+    await auth.signOut();
+  }
 }
